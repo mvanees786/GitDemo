3fc6d139b1c75ce202387c478c08ddc965fe509d
mvanees786 <mvanees786@gmail.com>
2024-02-05 15:51:33 +0530

    This is commit from Asian time zone

--- src/main/java/rahulshettyacademy/CucumberFramework/App.java
+++ src/main/java/rahulshettyacademy/CucumberFramework/App.java
@@ -3,5 +3,6 @@ package rahulshettyacademy.CucumberFramework;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Asian Hello World!");
   }
 }
